51b5e02b9cedf88298c67c556fa0947ae69a91dd
Akmal79 <109073778+Akmal79@users.noreply.github.com>
2024-03-25 16:29:25 +0530

    Add files via upload

---
+++ Unit1/day1/Errors.java
@@ -0,0 +1,10 @@
+public class Errors {
+        public static void main(String[] args)
+        {
+            System.out.println(0/0); 	//division by zero error
+            System.out.println(0.0 / 0);  	// NaN
+            System.out.println(1.0 / 0.0); 	//Infinity
+            System.out.println(-1.0 / 0.0); 	//-Infinity
+        }
+    }
+
