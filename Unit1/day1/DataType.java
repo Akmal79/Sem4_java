fecfa542be9db9927aa23e7a7922b1fc9e7aa19e
Akmal79 <109073778+Akmal79@users.noreply.github.com>
2024-03-25 16:26:00 +0530

    Add files via upload

---
+++ Unit1/day1/DataType.java
@@ -0,0 +1,11 @@
+public class DataType {
+    public static void main(String[] args)
+        {
+            int x = 123456;
+//            float f = 100.12F;
+            double f = 100.12;
+            System.out.println("Integer value="+x);
+            System.out.println("Float Value="+f);
+        }
+    }
+
